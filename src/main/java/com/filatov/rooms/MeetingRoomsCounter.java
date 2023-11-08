28ef60fddc1a85b8e0ed0ee84071d69419a0e004
Dmitrii <filatov2008@yandex.ru>
2023-11-08 21:46:22 +0300

    naming

--- src/main/java/com/filatov/rooms/MeetingRoomsCounter.java
+++ src/main/java/com/filatov/rooms/MeetingRoomsCounter.java
@@ -10,23 +10,29 @@ import java.util.List;
 @Service
 public class MeetingRoomsCounter {
 
+    private static final Integer START_MEETING_POSITION = 0;
+    private static final Integer END_MEETING_POSITION = 1;
+    private static final Integer EARLIEST_END_MEETING_POSITION = 0;
+
     public int countMinMeetingRooms(List<List<Integer>> meetingIntervals) {
         //sort intervals by meeting`s start time
         meetingIntervals = new ArrayList<>(meetingIntervals);
         Collections.sort(
                 meetingIntervals,
-                Comparator.comparing((List<Integer> interval) -> interval.get(0)));
+                Comparator.comparing((List<Integer> interval) -> interval.get(START_MEETING_POSITION)));
 
         List<Integer> roomsMeetingEndTime = new ArrayList<>();
         for (List<Integer> interval : meetingIntervals) {
             //sorting the end time of meetings in all involved rooms
             Collections.sort(roomsMeetingEndTime);
-            if (roomsMeetingEndTime.isEmpty() || interval.get(0).intValue() < roomsMeetingEndTime.get(0).intValue()) {
+            if (roomsMeetingEndTime.isEmpty() ||
+                    interval.get(START_MEETING_POSITION).intValue() <
+                            roomsMeetingEndTime.get(EARLIEST_END_MEETING_POSITION).intValue()) {
                 //We need more rooms
-                roomsMeetingEndTime.add(interval.get(1));
+                roomsMeetingEndTime.add(interval.get(END_MEETING_POSITION));
             } else {
                 //We don't need more rooms
-                roomsMeetingEndTime.set(0, interval.get(1));
+                roomsMeetingEndTime.set(EARLIEST_END_MEETING_POSITION, interval.get(END_MEETING_POSITION));
             }
         }
         return roomsMeetingEndTime.size();
